df7bfc60eadff7c8d3ae61727b024c41fe3db783
hyorim jo <133748490+hyorim-jo@users.noreply.github.com>
2025-06-06 23:59:04 +0900

    feat: 장소 상세 정보 연동 #47

--- src/components/PlaceInfo/PlaceInfoSimple.jsx
+++ src/components/PlaceInfo/PlaceInfoSimple.jsx
@@ -1,15 +1,16 @@
 import styles from './PlaceInfoSimple.module.css';
 
-const PlaceInfoSimple = ({ place, label }) => {
-	return (
-		<div className={styles.placeInfoSimple}>
-			<div className={styles.numbering}>{label}</div>
-			<div className={styles.info}>
-				<div className={styles.name}>{place.place_name}</div>
-				<div className={styles.address}>{place.roadAddress || place.address || '주소 정보 없음'}</div>
-			</div>
-		</div>
-	);
+const PlaceInfoSimple = ({ place, label, onClick }) => {
+  return (
+    <div className={styles.placeInfoSimple} onClick={onClick}>
+      <div className={styles.numbering}>{label}</div>
+      <div className={styles.info}>
+        <div className={styles.name}>{place.place_name}</div>
+        <div className={styles.address}>{place.roadAddress || '주소 없음'}</div>
+      </div>
+    </div>
+  );
 };
+
 
 export default PlaceInfoSimple;
--- src/features/LeftBar/SearchingPlace/PlaceInfo.jsx
+++ src/features/LeftBar/SearchingPlace/PlaceInfo.jsx
@@ -1,16 +1,16 @@
 import PlaceHeader from '../../../components/MakeRoute/PlaceHeader';
-import Search from '../../../components/Search/Search'
+import StartSearch from '../../../components/Search/StartSearch'
 import SearchingRoad from '../../../components/Button/SearchingRoad';
 import TransportModes from '../../../components/MakeRoute/TransportModes';
 import PlaceInfoCard from '../../../components/PlaceInfo/PlaceInfoCard';
 import RatingCard from '../../../components/Rating/RatingCard';
 import ReportFooter from '../../../components/ReportFooter/ReportFooter';
 
-const PlaceInfo = () => {
+const PlaceInfo = ( { place } ) => {
   return (
     <>
-      <PlaceHeader />
-      <Search />
+      <PlaceHeader place={place} />
+      <StartSearch />
       <SearchingRoad />
       <TransportModes />
       <PlaceInfoCard />
