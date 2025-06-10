f627547926e5f5c264f6a0c76e4f4b0fae7219b9
chaeyeon089 <chaeyeon0091@naver.com>
2025-06-10 18:52:02 +0900

    fix: 원래 파일로 되돌림

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-        // 필요하면 rewrite 설정도 추가
-      }
-    }
-  }
-});
+})
