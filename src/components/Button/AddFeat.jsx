5feda05399aade28a0de9a3500335ba9aa5cb8b0
hyorim jo <133748490+hyorim-jo@users.noreply.github.com>
2025-06-11 17:12:47 +0900

    feat: my Route연결 #82

--- src/components/Button/AddFeat.jsx
+++ src/components/Button/AddFeat.jsx
@@ -4,7 +4,7 @@ const AddFeat = () => {
     return (
         <div className={styles.container}>
             <div className={styles.featureBox}>
-                <div className={styles.featureText}>추가 기능 자리</div>
+                <div className={styles.featureText}>자유롭게 루트 탐방하기</div>
             </div>
         </div>
     );
